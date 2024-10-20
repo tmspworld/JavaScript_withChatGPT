11a23f85b8e64a670cecb41d7176b242081aaea8
tmspworld <gagamandeep@gmail.com>
2024-10-20 21:06:28 +0530

    ✅ S1 Variables and Constants

---
+++ All_Section_programs/Variables_and_Constants.js
@@ -0,0 +1,22 @@
+// 1. Variables and Constants
+// In JavaScript, we use var, let, and const to declare variables.
+
+
+// // JavaScript Code
+// let name = 'React Learner'; // let allows the value to be changed
+// const appName = 'React';    // const value cannot be changed
+            
+// - Use let for variables that may change.
+
+// - Use const for variables that shouldn’t change.
+
+// Example:
+
+// JavaScript Code
+let age = 25;
+const pi = 3.14;
+
+age = 26; // valid
+//pi = 3.15; // Error: Assignment to constant variable
+
+console.table(age);
